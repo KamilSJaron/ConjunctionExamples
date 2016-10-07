88ee23e69faa01ff4e7f9b073aff6a4331b90611
Kamil S Jaroň <376090@mail.muni.cz>
2016-10-07 19:39:32 +0200

    HW added sampling

--- 9_Hardy–Weinberg_principle/HW_evaluation.R
+++ 9_Hardy–Weinberg_principle/HW_evaluation.R
@@ -35,3 +35,37 @@ pdf('relation_HW_demesize.pdf')
        ylab = 'departure from HW',
        log='x')
 dev.off()
+
+# introducing sampling (I will draw 8 random individuals per simualtion)
+GradTable$sampled_AA <- 0
+GradTable$sampled_AB <- 0
+GradTable$sampled_BB <- 0
+
+for(i in 1:nrow(GradTable)){
+  haplotypes <- c(rep('AA',round(GradTable$AA[i] * GradTable$D[i])),
+  rep('AB',round(GradTable$AB[i] * GradTable$D[i])),
+  rep('BB',round(GradTable$BB[i] * GradTable$D[i])))
+  shaplotypes <- sample(haplotypes, 8, replace=F)
+  GradTable$sampled_AA[i] <- mean(shaplotypes == 'AA')
+  GradTable$sampled_AB[i] <- mean(shaplotypes == 'AB')
+  GradTable$sampled_BB[i] <- mean(shaplotypes == 'BB')
+}
+
+GradTable$sampled_A <- GradTable$sampled_AA + GradTable$sampled_AB / 2
+GradTable$sampled_B <- GradTable$sampled_BB + GradTable$sampled_AB / 2
+
+GradTable$sampled_AB_exp <- GradTable$sampled_A * GradTable$sampled_B  * 2
+GradTable$sampled_AA_exp <- GradTable$sampled_A * GradTable$sampled_A
+GradTable$sampled_BB_exp <- GradTable$sampled_B * GradTable$sampled_B
+
+GradTable$sampled_departure <- abs(GradTable$sampled_AA - GradTable$sampled_AA_exp) +
+                       abs(GradTable$sampled_AB - GradTable$sampled_AB_exp) +
+                       abs(GradTable$sampled_BB - GradTable$sampled_BB_exp)
+
+pdf('relation_HW_demesize_with_sampling.pdf')
+  plot(GradTable$sampled_departure ~ GradTable$D,
+       pch = 20,
+       xlab = expression(paste(log[2],' population size')),
+       ylab = 'departure from HW in 8 sampled individuals',
+       log='x')
+dev.off()
---
+++ 9_Hardy–Weinberg_principle/run.sh
@@ -0,0 +1,1 @@
+conjunction 1> HW.out 2> HW.err
