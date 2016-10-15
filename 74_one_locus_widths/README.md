3a603fad6b5e87b8cc116b3cd89f0f4dc646c69c
Kamil S Jaroň <376090@mail.muni.cz>
2016-10-15 15:42:00 +0200

    flowles mindflow - finally proportion of gneome acting together

---
+++ 74_one_locus_widths/README.md
@@ -0,0 +1,42 @@
+# One locus widths under fixed effect of drift
+
+So, we know that we do not have reliable estimate of cline widths under drift,
+or that the method we are using for estimation of width is not appropriate for
+wide clines.
+
+## one locus simulation.
+
+Well, we do not have the theory, so let's make some... Using ![setting](one_locus_setting.txt)
+we simulate a width of one locus clines with fixed drift (deme size is 16).
+The summary is saved every 100 generation and every simulation is replicated 5 times.
+
+Widths are estimated using HZAR and plotted in ![Fig 1](widths_vs_selection_L1.pdf).
+A line represent diffusion approximation.
+
+Taking means of widths allows us to create a dictionary of widths and corresponding
+selection of one locus clines under the fixed effect of drift.
+
+## multi locus simulation
+
+We re-simulate the dataset we had in ![](../73_searching_for_edge) with 5 replicates
+per simulation, but slightly narrower grid of parameters. There is a sharp
+edge (selection < 0.7 and beta < 0.25) of simulations which have not converged
+within the first 100 generations and those that quickly converged. For visualisation
+purposes, selection and beta was recalculated to integral of the fitness function
+as `1 - (4^beta * selection * gamma(beta + 1)^2) / (gamma(2 * beta + 2))`, where
+`gamma` is a Gamma function. Color code shows a generation of a measurement, ![Fig 2](multilocus_widths_vs_AUFC_and_generation).
+
+## Effective proportion of genome acting together
+
+So we simulated widths of multilocus clines under several conditions,
+now we can find selections needed to generate those widths in one locus clines
+using dictionary created above.
+The proportion of the corresponding one locus selection to
+actual selection used for simulation then gives us a proportion of genome
+acting together (See ![Fig 3](proportion_of_genome_vs_selection.pdf)). Few values
+are slightly over one, which is probably just noise in width estimation
+of steep clines (in fact combination of noise in estimation of single locus clines
+and multilocus clines, which I tried to minimize by using 5 replicates and all
+time points).
+
+I should probably simulate also some values exactly on the edge.
--- 74_one_locus_widths/one_locus_drift_widths.R
+++ 74_one_locus_widths/one_locus_drift_widths.R
@@ -1,41 +1,73 @@
 library(ConjunctionStats)
 library(RColorBrewer)
 
-GradTable <- ReadSetting()
-sim <- ReadSummary('one_locus_widths.out')
+GradTable_L1 <- ReadSetting('one_locus_setting.txt')
+sim_L1 <- ReadSummary('one_locus_widths.out')
+GradTable_L1 <- FillSettingByHZAR(sim_L1,GradTable_L1)
+write.table(GradTable_L1, 'one_locus_widths.tsv')
+# GradTable_L1 <- read.table('one_locus_widths.tsv')
+
+#GradTable_L1 <- GradTable_L1[GradTable_L1$G == 1000,]
+
+pdf('widths_vs_selection_L1.pdf')
+GradTable_L1_means <- data.frame(s = numeric(0), b = numeric(0), L1_width_H = numeric(0))
+
+x <-seq(0,1,by=0.01)
+
+plot(x,twidth(x,sqrt(0.5)), type = 'l',
+     ylim = c(min(GradTable_L1$width_H), max(GradTable_L1$width_H)),
+     xlim = c(min(GradTable_L1$s), max(GradTable_L1$s)),
+     xlab = 'selection', ylab = 'width_H')
+
+for(sel in unique(GradTable_L1$s)){
+    mw <- mean(GradTable_L1$width_H[GradTable_L1$s == sel])
+    GradTable_L1_means <- rbind(GradTable_L1_means, data.frame(s = sel, L1_width_H = mw))
+    boxplot(GradTable_L1$width_H[GradTable_L1$s == sel],
+            at = sel,
+            boxwex = 0.05,
+            add = T)
+}
+dev.off()
+
+GradTable <- ReadSetting('many_loci_setting.txt')
+sim <- ReadSummary('many_loci_widths.out')
 GradTable <- FillSettingByHZAR(sim,GradTable)
-write.table(GradTable, 'one_locus_widths.tsv')
+# write.table(GradTable, 'many_loci_widths.tsv')
+# GradTable <- read.table('many_loci_widths.tsv')
+# GradTable <- GradTable[GradTable$G == 1000,]
 
-GraTable_L1_medians <- data.frame(s = numeric(0), b = numeric(0), L1_width_H = numeric(0))
+GradTable$AUFC <- getAUFC(GradTable$s, GradTable$b)
+pal <- brewer.pal(10,'RdYlBu')
+pdf('multilocus_widths_vs_AUFC_and_generation.pdf')
+  PlotStat(GradTable, stat = 'width_H', par1 = 'AUFC', par2 = 'G',
+           legend_position = 'topleft', pal = pal, main = 'multilocus cline',
+           ylab = 'Cline width', xlab = 'Area under fitness function')
+dev.off()
+
+GradTable_L20k_means <- data.frame(s = numeric(0),
+                                    b = numeric(0),
+                                    width_H = numeric(0),
+                                    closest_1L_s = numeric(0))
 for(sel in unique(GradTable$s)){
   for(beta in unique(GradTable$b)){
-    mw <- median(GradTable$width_H[GradTable$s == sel & GradTable$b == beta])
-    GraTable_L1_medians <- rbind(GraTable_L1_medians, data.frame(s = sel, b = beta, L1_width_H = mw))
+    # mean because some valuas do not converge in first 100 generations,
+    # so I wanted to take a meansure with takes into account outlayers
+    mw <- mean(GradTable$width_H[GradTable$s == sel & GradTable$b == beta])
+    cs <- GradTable_L1_means$s[which.min(abs(GradTable_L1_means$L1_width_H - mw))]
+    GradTable_L20k_means <- rbind(GradTable_L20k_means,
+                                   data.frame(s = sel,
+                                              b = beta,
+                                              width_H = mw,
+                                              closest_1L_s = cs))
   }
 }
 
-GradTable <- read.table('../73_searching_for_edge/width_estimates.tsv')
-GraTable_L20k_medians <- data.frame(s = numeric(0), b = numeric(0), width_H = numeric(0))
-for(sel in unique(GradTable$s)){
-  for(beta in unique(GradTable$b)){
-    mw <- median(GradTable$width_H[GradTable$s == sel & GradTable$b == beta])
-    GraTable_L20k_medians <- rbind(GraTable_L20k_medians, data.frame(s = sel, b = beta, width_H = mw))
-  }
-}
+GradTable_L20k_means$sss <- GradTable_L20k_means$closest_1L_s / GradTable_L20k_means$s
 
-colnames(GraTable_L1_medians)[3] <- 'L1_width_H'
-GradTable <- merge(GraTable_L20k_medians, GraTable_L1_medians)
-
-GradTable$ss_L1 <- 4 / GradTable$L1_width_H^2
-GradTable$ss_H <- 4 / GradTable$width_H^2
-
-GradTable$sss <- GradTable$ss_H / GradTable$ss_L1
-
-pal <- brewer.pal(7,'RdYlGn')
-
-pdf('sss_agains_1L_equivalent.pdf')
-PlotStat(GradTable, stat = 'sss', par1 = 's', par2 = 'b',
-         legend_position = 'topleft', pal = pal)
+pal <- brewer.pal(3,'RdYlGn')
+pdf('proportion_of_genome_vs_selection.pdf')
+  PlotStat(GradTable_L20k_means, stat = 'sss', par1 = 's', par2 = 'b',
+           legend_position = 'bottomright', pal = pal, xlab = 's* / S')
 dev.off()
 
 #GradTable$AUFC <- getAUFC(GradTable$s, GradTable$b)
