f25dbe42dbab715997f5b2c5e94358c3df6d643b
Kamil S Jaroň <376090@mail.muni.cz>
2016-10-20 16:52:04 +0200

    75: figures updates with datapoints for s=0.6 and D = 16

--- 75_effects_of_drift/effect_of_drift_and_dispersal_on_phasechange.R
+++ 75_effects_of_drift/effect_of_drift_and_dispersal_on_phasechange.R
@@ -26,7 +26,7 @@ onelocus_LM_D32 <- getReplicateAverages(onelocus_LM_D32)
 multilocus_LM_D32 <- fillClosestS(multilocus_LM_D32, onelocus_LM_D32)
 
 # sigma^2 = 0.5, D=16
-multilocus_HM_D16 <- read.table('../74_one_locus_widths/many_loci_widths.tsv')
+multilocus_HM_D16 <- read.table('../74_one_locus_widths/multilocus.tsv')
 onelocus_HM_D16 <- read.table('../74_one_locus_widths/one_locus_widths.tsv')
 #multilocus_HM_D16 <- getReplicateAverages(multilocus_HM_D16)
 onelocus_HM_D16 <- getReplicateAverages(onelocus_HM_D16)
@@ -96,3 +96,32 @@ pdf('sss_vs_demesize.pdf')
          col = D32col, pch = 15)
   legend('topright', c('16', '32', 'overlap', 'mean'), pch = c(rep(20,3),15), col = c(D16col, D32col, rgb(0.6,0.2,0.6), 'black'))
 dev.off()
+
+GradTable <- rbind(multilocus_HM_D32, multilocus_LM_D32, multilocus_HM_D16)
+GradTable$sss <- GradTable$sss + rnorm(nrow(GradTable), 0, 0.005)
+GradTable$AUFC <- getAUFC(GradTable$s, GradTable$b) + rnorm(nrow(GradTable), 0, 0.007)
+pdf('sss_vs_AUFC_D.pdf')
+  PlotStat(GradTable, stat = 'sss', par1 = 'AUFC', par2 = 'D',
+          legend_position = 'bottomleft', pal = c(D16col, D32col), add = F,
+          xlab = 's + N(0,0.007)', ylab = '(s* / S) + N(0,0.005)')
+dev.off()
+
+# from sss_vs_AUFC_D we see that we need AUFCs between 0.38 and 0.4 to be on the edge...
+# create lots of combinations of selections and beta in whole reasonable range
+selelctions <- rep(seq(0.001,1,length = 1000), 1000)
+betas <- rep(seq(1, 0.001, length = 1000), each = 1000)
+# get their AUFC
+AUFCs <- getAUFC(selelctions,betas)
+
+# select those that have AUFC in desired range
+AUFCs_chosen <- which(AUFCs > 0.38 & AUFCs < 0.40)
+# plot combinaitons of seletions and beta that are in the range
+plot(selelctions[AUFCs_chosen] ~ betas[AUFCs_chosen], pch = 20, cex = 0.01)
+
+# we see that for selection in range 0.62 ti 0.9 there is always a range of betas with AUFC in the range
+# so for 8 equdistant values in range 0.62 to 0.9 we get a median of betas
+# so we see a combinations of selections and betas, that still give AUFC between 0.38 and 0.4 with the
+# greatest possible distance of selections / beta, see simulation 77 to find more
+for(selection in seq(0.62, 0.9, by = 0.04)){
+  print(median(betas[AUFCs_chosen][round(selelctions[AUFCs_chosen], 3) == round(selection,3)][-1]))
+}
