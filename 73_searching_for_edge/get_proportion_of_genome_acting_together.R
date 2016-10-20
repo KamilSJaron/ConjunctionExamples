a37c258ee1d21dc9589f86b49b70f4aa8eb1e02c
Kamil S Jaroň <376090@mail.muni.cz>
2016-10-20 19:37:39 +0200

    73 recalculated using approach in 74 and 75

--- 73_searching_for_edge/get_proportion_of_genome_acting_together.R
+++ 73_searching_for_edge/get_proportion_of_genome_acting_together.R
@@ -1,14 +1,14 @@
 library(ConjunctionStats)
 
-GradTable <- ReadSetting()
-sim <- ReadSummary('introblocks_73.out')
+#GradTable <- ReadSetting()
+#sim <- ReadSummary('introblocks_73.out')
 #GradTable <- FillSetting(sim,GradTable)
-GradTable <- FillSettingByHZAR(sim,GradTable)
+#GradTable <- FillSettingByHZAR(sim,GradTable)
 # sim <- FillSummary(sim,GradTable$center)
 
-GradTable_zoomed <- ReadSetting('setting_zoom.txt')
-sim_zoomed <- ReadSummary('edge_zoomed.out')
-GradTable_zoomed <- FillSettingByHZAR(sim_zoomed,GradTable_zoomed)
+#GradTable_zoomed <- ReadSetting('setting_zoom.txt')
+#sim_zoomed <- ReadSummary('edge_zoomed.out')
+#GradTable_zoomed <- FillSettingByHZAR(sim_zoomed,GradTable_zoomed)
 
 GradTable <- rbind(GradTable, GradTable_zoomed)
 
@@ -74,4 +74,33 @@ pal <- brewer.pal(7,'RdYlGn')
 pdf('propotion_of_selection_vs_selection.pdf')
   PlotStat(GradTable, stat = 'propotion_of_selection_in_the_center',
   par1 = 's', par2 = 'b', 'topleft', pal = pal)
+dev.off()
+
+GradTable_old <- read.table('old_width_estimates.tsv')
+GradTable_old <- GradTable_old[GradTable_old$D == 32,]
+colnames(GradTable_old)[8] <- "width_old_H"
+GradTable_old <- GradTable_old[,c(-9,-10,-11)]
+GradTable_new <- read.table('width_estimates.tsv')
+
+
+source('../75_effects_of_drift/filling_functions.R')
+onelocus_HM_D32 <- read.table('../75_effects_of_drift/one_locus_D32_LM.tsv')
+onelocus_HM_D32 <- getReplicateAverages(onelocus_HM_D32)
+GradTable_new <- fillClosestS(GradTable_new, onelocus_HM_D32)
+
+GradTable_new$sss <- GradTable_new$ss / GradTable_new$s
+GradTable_new$sss <- GradTable_new$sss + rnorm(nrow(GradTable_new), 0, 0.005)
+GradTable_new$s <- GradTable_new$s + rnorm(nrow(GradTable_new), 0, 0.007)
+
+source('../scripts/add_alpha.R')
+
+pdf('sss_vs_sel_beta.pdf')
+  nbetas <- length(unique(GradTable_new$b))
+  pal <- add.alpha(brewer.pal(nbetas,'Spectral')[1], 0.65)
+  for(i in 2:nbetas){
+      pal <- c(pal, add.alpha(brewer.pal(nbetas,'Spectral')[i], 0.65))
+  }
+  PlotStat(GradTable_new, stat = 'sss', par1 = 's', par2 = 'b',
+           legend_position = 'bottomright', pal = pal, add = F,
+           xlab = 's + N(0,0.007)', ylab = '(s* / S) + N(0,0.005)')
 dev.off()
