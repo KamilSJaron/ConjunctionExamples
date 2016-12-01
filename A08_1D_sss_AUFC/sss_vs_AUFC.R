27fdda0b2c98b11afc9eeddfe4e8acf314eb8649
Kamil S Jaroň <376090@mail.muni.cz>
2016-12-01 20:32:41 +0100

    sss_vs_AUFC; AUFC in mathematica notebook

--- A08_1D_sss_AUFC/sss_vs_AUFC.R
+++ A08_1D_sss_AUFC/sss_vs_AUFC.R
@@ -1,7 +1,7 @@
 library(ConjunctionStats)
 library(RColorBrewer)
 
-small_s <- read.table('../75_effects_of_drift/setting_onelocus_D32.tsv')
+small_s <- read.table('../A05_1D_sss_under_drift/setting_onelocus_D32.tsv')
 small_s <- small_s[small_s$s < 0.1,]
 onelocus_HM_D32 <- rbind(read.table('onelocus.tsv'), small_s)
 onelocus_HM_D32 <- GetReplicateAverages(onelocus_HM_D32, 'one_locus_D32_HM_widths.pdf', filter = 1)
@@ -22,13 +22,13 @@ pdf('sss_vs_sel_beta.pdf')
   pal <- brewer.pal(8,'Spectral')
   PlotAverages(GradTable, 'sss', 's', 'b', pal)
   legend('topright', col = c(NA,pal), legend = c(expression(beta), unique(GradTable[,'b'])),
-         pch = 20, horiz = T, cex = 0.785)
+         pch = 20, horiz = T, cex = 0.785, bty = "n")
 dev.off()
 
 pdf('sss_vs_beta_s.pdf')
   pal <- adjustcolor(brewer.pal(8,'Spectral'), 0.3)
   PlotStat(GradTable, stat = 'sss', par1 = 'b_norm', par2 = 's',
-           legend_position = NA, pal = pal, add = F,
+           legend_position = NA, pal = pal, add = F, bty = "n",
            xlab = paste0(expression(beta),' + N(0,0.007)'), ylab = '(s* / S) + N(0,0.005)')
   pal <- brewer.pal(8,'Spectral')
   PlotAverages(GradTable, 'sss', 'b', 's', pal)
@@ -37,17 +37,43 @@ pdf('sss_vs_beta_s.pdf')
 dev.off()
 
 GradTable$AUFC <- GetAUFC(GradTable$s, GradTable$b)
-GradTable$AUFC_norm <- GradTable$AUFC + rnorm(nrow(GradTable), 0, 0.008)
-pdf('S5_sss_vs_AUFC_beta.pdf')
-  pal <- adjustcolor(brewer.pal(8,'Spectral'), 0.3)
-  PlotStat(GradTable, stat = 'sss', par1 = 'AUFC_norm',
-           par2 = 'b', legend_position = F, pal = pal, add = F,
-           xlab = 'AUFC + N(0,0.008)', ylab = '(s* / S) + N(0,0.005)')
-  pal <- adjustcolor(brewer.pal(8,'Spectral'))
-  PlotAverages(subset(GradTable, GradTable$G == 500), stat = 'sss',
-               par1 = 'AUFC', par2 = 'b', pal = pal)
-  legend('topright', col = pal, legend = unique(GradTable[,'b']),
-         title = expression(beta), pch = 20)
+GradTable$AUFC_norm <- GradTable$AUFC + rnorm(nrow(GradTable), 0, 0.002)
+
+stat = 'sss'
+par1 = 'AUFC_norm'
+par2 = 'b'
+par3 = 's'
+
+pdf('SX_sss_vs_AUFC.pdf')
+  pal <- brewer.pal(8,'Spectral')
+  t_pal <- adjustcolor(pal, 0.5)
+
+  xlim = c(min(GradTable[, par1]), max(GradTable[, par1]))
+  ylim = c(min(GradTable[, stat], na.rm = T), max(GradTable[, stat], na.rm = T))
+
+  plot(numeric(0), xlim = xlim, ylim = ylim, xlab = 'AUFC + N(0,0.002)',
+       ylab = 's* / s + N(0,0.005)')
+
+  for (par_state in unique(GradTable[, par2])) {
+    col <- pal[which(par_state == unique(GradTable[, par2]))]
+    t_col <- t_pal[which(par_state == unique(GradTable[, par2]))]
+    for (par3_state in unique(GradTable[, par3])){
+      pch <- 6 + which(par3_state == unique(GradTable[, par3]))
+
+      SubTable <- subset(GradTable, GradTable[, par2] == par_state &
+                                    GradTable[, par3] == par3_state)
+      points(SubTable[, par1], SubTable[, stat], col = t_col, pch = pch)
+      points(tapply(SubTable[, stat], SubTable[, 'AUFC'], mean) ~
+             sort(unique(SubTable[, 'AUFC'])), col = col, pch = 20, cex = 1.5)
+    }
+    SubTable <- subset(GradTable, GradTable[, par2] == par_state)
+    lines(tapply(SubTable[, stat], SubTable[, 'AUFC'], mean) ~
+          sort(unique(SubTable[, 'AUFC'])), col = col, lwd = 2)
+  }
+  legend(0.52, 1.55, col = pal, legend = unique(GradTable[,'b']),
+         title = expression(beta), pch = 20, bty = "n")
+  legend(0.46, 1.55, col = 1, legend = rev(unique(GradTable[,'s'])),
+        title = 'sel', pch = rev(seq(7, by = 1, length = 8)), bty = "n")
 dev.off()
 
 library(ggplot2)
